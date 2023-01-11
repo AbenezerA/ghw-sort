caf76882b30ff0c89b79b08d44f51d55cd387a8b
AbenezerA <ata2152@columbia.edu>
2023-01-11 12:30:51 -0500

    split up merge and sort

--- sort.py
+++ sort.py
@@ -1,4 +1,4 @@
-sorted = []
+import merge
 
 def sorter(list):
     list_len = len(list)
@@ -7,27 +7,7 @@ def sorter(list):
 
     left = sorter(list[:list_len // 2])
     right = sorter(list[list_len // 2:])
-    return merger(left, right)
-
-def merger(left, right):
-    sorted = []
-    len_left = len(left)
-    len_right = len(right)
-
-    i = 0; j = 0
-    while i < len_left and j < len_right:
-        if left[i] < right[j]:
-            sorted.append(left[i])
-            i += 1
-        else:
-            sorted.append(right[j])
-            j += 1
-    if i < len_left:
-        sorted.extend(left[i:])
-    else:
-        sorted.extend(right[j:])
-    
-    return sorted
+    return merge.merger(left, right)
 
 list = [3, 5, 7, 9, 2, 1, 8, 4]
-print(sorter(list)) # Succesfully prints 
+print(sorter(list))
